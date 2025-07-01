512d25f1bfb36e4487ed4b93985e72f413c9b93e
Chamod_Sandeepa <144690578+chabi1773@users.noreply.github.com>
2025-07-01 16:02:22 +0530

    Update App.test.js

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,8 +1,16 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import { MemoryRouter } from 'react-router-dom';
+import { AuthProvider } from './AuthContext';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders login page by default if not authenticated', () => {
+  render(
+    <AuthProvider>
+      <MemoryRouter initialEntries={['/dashboard']}>
+        <App />
+      </MemoryRouter>
+    </AuthProvider>
+  );
+
+  expect(screen.getByText(/login/i)).toBeInTheDocument();
 });
