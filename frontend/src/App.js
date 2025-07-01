8349098b3d9d2adc3012f814218e6cde21802da1
Chamod_Sandeepa <144690578+chabi1773@users.noreply.github.com>
2025-07-01 16:27:22 +0530

    patient -> patients

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -24,7 +24,7 @@ function App() {
             } 
           />
           <Route 
-            path="/patient/:id" 
+            path="/patients/:id" 
             element={
               <PrivateRoute>
                 <PatientDetails />
