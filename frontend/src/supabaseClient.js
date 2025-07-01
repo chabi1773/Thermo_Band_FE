8518b9221a0d51ef6aa9bf32a35c6aad7542dbca
Chamod_Sandeepa <144690578+chabi1773@users.noreply.github.com>
2025-07-01 09:40:31 +0530

    db changed

--- frontend/src/supabaseClient.js
+++ frontend/src/supabaseClient.js
@@ -1,5 +1,5 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://fcqvzjwskiqpkposftet.supabase.co'; // replace with your Supabase URL
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZjcXZ6andza2lxcGtwb3NmdGV0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDg0MTE0MTIsImV4cCI6MjA2Mzk4NzQxMn0.VWpm22PHYzQSFCP7Vqzv96EHn-Q2AwUj9luPA1LtOGA'; // replace with your anon/public key
+const supabaseUrl = 'https://ntcsljlsxmekjdsjesri.supabase.co'; // replace with your Supabase URL
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im50Y3NsamxzeG1la2pkc2plc3JpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTEyNzQwMDYsImV4cCI6MjA2Njg1MDAwNn0.vR5_Fhvks-57r-J7qOQdvpvkBL8_7vIiQ8r7dJx8T_k'; // replace with your anon/public key
 export const supabase = createClient(supabaseUrl, supabaseKey);
