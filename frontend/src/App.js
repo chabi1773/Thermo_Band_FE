d74674016a6bf00f3353ed50db4665b7edda201d
Chamod_Sandeepa <144690578+chabi1773@users.noreply.github.com>
2025-07-02 19:48:07 +0530

    bg colorrrr

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,43 +7,45 @@ import Login from './components/Login';
 import Signup from './components/Signup';
 import Dashboard from './components/Dashboard';
 import PatientDetails from './components/PatientDetails';
-import AddPatient from './components/AddPatient'; // Import AddPatient
+import AddPatient from './components/AddPatient';
 
 function App() {
   return (
-    <AuthProvider>
-      <Router>
-        <Routes>
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route
-            path="/dashboard"
-            element={
-              <PrivateRoute>
-                <Dashboard />
-              </PrivateRoute>
-            }
-          />
-          <Route
-            path="/patients/:id"
-            element={
-              <PrivateRoute>
-                <PatientDetails />
-              </PrivateRoute>
-            }
-          />
-          <Route
-            path="/add-patient"    // New route here
-            element={
-              <PrivateRoute>
-                <AddPatient />
-              </PrivateRoute>
-            }
-          />
-          <Route path="*" element={<Login />} />
-        </Routes>
-      </Router>
-    </AuthProvider>
+    <div style={{ minHeight: '100vh', backgroundColor: '#c2cbb3' }}>
+      <AuthProvider>
+        <Router>
+          <Routes>
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />
+            <Route
+              path="/dashboard"
+              element={
+                <PrivateRoute>
+                  <Dashboard />
+                </PrivateRoute>
+              }
+            />
+            <Route
+              path="/patients/:id"
+              element={
+                <PrivateRoute>
+                  <PatientDetails />
+                </PrivateRoute>
+              }
+            />
+            <Route
+              path="/add-patient"
+              element={
+                <PrivateRoute>
+                  <AddPatient />
+                </PrivateRoute>
+              }
+            />
+            <Route path="*" element={<Login />} />
+          </Routes>
+        </Router>
+      </AuthProvider>
+    </div>
   );
 }
 
